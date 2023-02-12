3e3ad142a0f0788bedfa4800541277e98875b6c4
Aamer Sohail <amersohailroot@gmail.com>
2023-02-12 16:29:40 +0530

    initial Commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,60 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ favrite ? '(favrite)': '' }}</h2>
+    <button @click="isFavToggle">Toggle favrite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props:{
+    name:{
+      type:String,
+      required:true,
+    },
+    emailAddress:{
+      type:String,
+      required:true,
+    },
+    phoneNumber:{
+      type:String,
+      required:true,
+    },
+    isFavarite:{
+      type:Boolean,
+      required:false,
+      default:false,
+      // validator:function(value){
+      //   return  value==='0' | value==='1';
+      // }
+    }
+
+  }
+  ,
+  data() {
+    return {
+      detailsAreVisible: false,
+      favrite:this.isFavarite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    isFavToggle(){
+      this.favrite=!this.favrite;
+    }
+  }
+};
+</script>
